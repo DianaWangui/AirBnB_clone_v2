23478d14fef4ad80a6ae38ce223a1a321590e278
DianaWangui <dianawanguikt048@gmail.com>
2024-03-26 08:51:29 +0300

    update with sqlalchemy codes

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship, back_populates
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel):
-    name = ""
+    """ A place to stay """
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    place_amenities = relationship('Place')
