09e71a169282134b8bded3857b14599edce4e531
DianaWangui <dianawanguikt048@gmail.com>
2024-04-05 00:23:00 +0300

    update the script with sudo tee function

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers for the deployment of web_static
+# Install nginx and create directories
+sudo apt-get update
+sudo apt-get install -y nginx
 
-# update package managers
-sudo apt-get -y update
-
-# install nginx
-sudo apt-get -y install nginx
-
-#creating directories
 directories=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
+
 for dir in "${directories[@]}"; do
     if [ ! -d "$dir" ]; then
         sudo mkdir -p "$dir"
     fi
 done
 
-# create html file
-sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
+sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
 <html>
   <head>
   </head>
@@ -26,22 +21,11 @@ sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
 </html>
 EOF
 
-# creating symbolic link
-target="/data/web_static/releases/test/"
-link="/data/web_static/current"
+sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
 
-# Check if the symbolic link already exists and delete it
-if [ -L "$link" ]; then
-    sudo rm -r "$link"
-fi
+sudo chwon -R ubuntu:ubuntu /data/
 
-# Create the symbolic link
-sudo ln -sfn "$target" "$link"
-
-# giving owndership of data directory to user and group ubuntu
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo tee /etc/nginx/sites-available/default >/dev/null <<EOF
+sudo tee /etc/nginx/sites-available/default > /dev/null <<EOF
 server {
         listen 80;
         listen [::]:80 default_server;
@@ -63,5 +47,4 @@ server {
         error_page 404 = @404;
 }
 EOF
-
 sudo service nginx restart
