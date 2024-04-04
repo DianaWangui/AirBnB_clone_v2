3a1912ae6fcd2efaf5c38cbdeca9bd6caea588e4
DianaWangui <dianawanguikt048@gmail.com>
2024-04-05 00:51:26 +0300

    update the script with sudo tee function

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,17 +4,16 @@ sudo apt-get -y update
 sudo apt-get install -y nginx
 
 # directories and files 
+#creating directories
 directories=("/data/" "/data/web_static/" "/data/web_static/releases/" "/data/web_static/shared/" "/data/web_static/releases/test/")
-
-# loops through to check if they exist if not create them
 for dir in "${directories[@]}"; do
     if [ ! -d "$dir" ]; then
         sudo mkdir -p "$dir"
     fi
 done
 
-# create a fake html file and append to the index.html file
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
+# create html file
+sudo tee /data/web_static/releases/test/index.html > /dev/null << EOF
 <html>
   <head>
   </head>
@@ -36,10 +35,9 @@ fi
 # Create the symbolic link
 sudo ln -sfn "$target" "$link"
 
-# change ownership of the /data/ folder
-sudo chwon -R ubuntu:ubuntu /data/
+# giving owndership of data directory to user and group ubuntu
+sudo chown -R ubuntu:ubuntu /data/
 
-# update the default file
 sudo tee /etc/nginx/sites-available/default >/dev/null <<EOF
 server {
         listen 80;
