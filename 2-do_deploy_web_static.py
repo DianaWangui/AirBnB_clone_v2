2e5c1db1789aaffd98dd0dbf38e014013b25d252
DianaWangui <dianawanguikt048@gmail.com>
2024-04-05 17:08:01 +0300

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from the
+contents of the web_static folder of your AirBnB Clone repo."""
+import os
+from fabric.api import *
+from datetime import datetime
+
+env.hosts = ['54.237.118.67', '52.91.146.16']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_pack():
+    """Function to generate a .tgz archive"""
+    # Creating the folder versions
+    local('sudo mkdir -p versions')
+
+    # Getting the current time
+    time_now = datetime.now()
+    str_time = time_now.strftime("%Y%m%d%H%M%S")
+
+    local(f'sudo tar -cvzf versions/web_static_{str_time}.tgz web_static')
+
+    file_path = f"versions/web_static_{str_time}.tgz"
+    file_size = os.path.getsize(file_path)
+
+    if os.path.exists(file_path):
+        print(f"web_static packed: {file_path} -> {file_size}Bytes")
+    else:
+        return None
+
+# Path: 2-do_deploy_web_static.py
+def do_deploy(archive_path):
+    """
+    Fabric script (based on the file 1-pack_web_static.py
+    that distributes an archive to your web servers, using
+    the function do_deploy.
+    """
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive_name = archive_path.split("/")[-1]
+        file_name = archive_name.split(".")[0]
+
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, "/tmp/")
+
+        # Create the directory to uncompress the file
+        run(f"sudo mkdir -p /data/web_static/releases/{archive_name}")
+
+        # Uncompress the file into the created directory
+        run(f"sudo tar -xzf /tmp/{archive_name} -C /data/web_static/releases/{file_name}")
+
+        # Delete the archive from the web server
+        run(f"sudo rm /tmp/{archive_name}")
+
+
+        # Delete the symbolic link
+        run("sudo rm -rf /data/web_static/current")
+
+        # Create a new symbolic link
+        run(f"sudo ln -s /data/web_static/releases/"{file_name}"/ /data/web_static/current")
+
+        # Finish the deployment
+        print("New version deployed!")
+        return True
+
+    except:
+        return False
