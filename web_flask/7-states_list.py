c7a7b131216ff79351cd2f4805bf02c82c15ca3e
DianaWangui <dianawanguikt048@gmail.com>
2024-04-22 00:21:52 +0300

    make file executable

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,12 +5,12 @@ from models import storage
 from models.state import State
 
 app = Flask(__name__)
+states = storage.all(State)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Display a HTML page."""
-    states = storage.all(State)
     return render_template('7-states_list.html', states=states)
 
 
