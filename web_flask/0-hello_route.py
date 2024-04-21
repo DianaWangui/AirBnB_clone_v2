28e2ee8312a0c938f8d2da76d6a91d42181cc85e
DianaWangui <dianawanguikt048@gmail.com>
2024-04-21 15:50:06 +0300

    add the strict slash

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""
+This module supplies a script that starts a flask web application
+"""
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Displays Hello HBNB!
+    """
     return "Hello HBNB!"
 
 
